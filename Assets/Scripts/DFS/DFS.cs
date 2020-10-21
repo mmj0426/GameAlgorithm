7681dfc2292d7a044f6c8b3a00794e4add5261d4
mmj0426 <52391149+mmj0426@users.noreply.github.com>
2020-10-21 19:42:35 +0900

    DFS 구현
    경로 출력은 아직..

--- Assets/Scripts/DFS/DFS.cs
+++ Assets/Scripts/DFS/DFS.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Transactions;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class DFS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Graph graph;
+
+    private Node startNode;
+    private Node endNode;
+
+
+    public void onClick()
     {
-        
+        if (graph.IsRun) { return; }
+
+        graph.IsRun = true;
+
+        startNode = graph.startNode;
+        endNode = graph.endNode;
+        Debug.Log("DFS - Start : " + startNode + " End : " + endNode);
+
+        //StartCoroutine("Active", startNode);
+
+        Active(startNode);
+
+        //graph.IsRun = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Active(Node _node)
     {
+        _node.isVisited = true;
+
+        Debug.Log("DFS : " + _node);
+
+        _node.GetComponent<Renderer>().material.color = Color.green;
+
+        int current = Convert.ToInt32(_node.name);
+
+        if (endNode == _node)
+        {
+            _node.GetComponent<Renderer>().material.color = Color.blue;
+            
+        }
+
+        foreach (var v in graph.nodeList[current].nodeDic)
+        {
+            if (!v.Key.isVisited)
+            {
+                Active(v.Key);
+            }
+        }
         
     }
 }
